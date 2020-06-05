e0c305590d28e8c9ca19c0a0d236ff3ca07b29fb
marcio henrique <mhmuzy@embratel.com.br>
2020-06-05 11:14:22 -0300

    Treinamento

---
+++ Projeto.Infra.Data/Context/SqlContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Projeto.Domain.Models;
+using Projeto.Infra.Data.Mappings;
+
+namespace Projeto.Infra.Data.Context
+{
+    //Regra 1) Herdar DbContext
+    public class SqlContext : DbContext
+    {
+        //Regra 2) Construtor para receber as configurações para
+        //acesso ao banco de dados do SqlServer
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) {
+        
+        }
+
+        //Regra 3) Declarar um DbSet (CRUD) para cada modelo de dados
+        public DbSet<Produto> Produtos { get; set; }
+
+        //Regra 4) Sobrescrita (OVERRIDE) do método OnModelCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //adicionar as classes de mapeamento
+            modelBuilder.ApplyConfiguration(new ProdutoMap());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Projeto.Infra.Data/Context/SqlContextFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projeto.Infra.Data.Context
+{
+    class SqlContextFactory
+    {
+    }
+}
